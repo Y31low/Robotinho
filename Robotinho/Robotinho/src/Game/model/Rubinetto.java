3bb1847bffec0e27a56d62c3cd42941d8e8efee1
Y31low <samuele.giallorenzo@gmail.com>
2023-07-31 17:54:05 +0200

    Changes by fedem

--- Robotinho/Robotinho/src/Game/model/Rubinetto.java
+++ Robotinho/Robotinho/src/Game/model/Rubinetto.java
@@ -1,11 +1,15 @@
 package Game.model;
 
+import java.util.ArrayList;
+
 public class Rubinetto extends Casella implements Rompibile{
     private boolean stato;
+    private ArrayList<Casella> vicini;
 
     public Rubinetto(int posizionex, int posizioney) {
         super(posizionex, posizioney);
         this.stato = false;
+        vicini = new ArrayList<>();
     }
 
     public String toString() {
@@ -14,7 +18,27 @@ public class Rubinetto extends Casella implements Rompibile{
 
     @Override
     public void perdita() {
-
+        Pavimento stato;
+        if (this.vicini.get(0).toString().equals("Pavimento")) {
+            stato = (Pavimento) vicini.get(0);
+            if (!stato.getStato())
+                stato.setStato(true);
+        }
+        if (this.vicini.get(1).toString().equals("Pavimento")) {
+            stato = (Pavimento) vicini.get(1);
+            if (!stato.getStato())
+                stato.setStato(true);
+        }
+        if (this.vicini.get(2).toString().equals("Pavimento")) {
+            stato = (Pavimento) vicini.get(2);
+            if (!stato.getStato())
+                stato.setStato(true);
+        }
+        if(this.vicini.get(3).toString().equals("Pavimento")) {
+            stato = (Pavimento) vicini.get(3);
+            if (!stato.getStato())
+                stato.setStato(true);
+        }
     }
 
     @Override
