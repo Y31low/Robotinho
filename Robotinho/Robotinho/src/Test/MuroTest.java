2abab571a13c5719fe1d4be2d02195cb7af908e3
Y31low <samuele.giallorenzo@gmail.com>
2023-08-25 16:00:29 +0200

    perchè vuoi sempre un messaggio?

--- Robotinho/Robotinho/src/Test/MuroTest.java
+++ Robotinho/Robotinho/src/Test/MuroTest.java
@@ -1,10 +1,17 @@
 package Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import Game.Model.Muro;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 
 class MuroTest {
-
+    Muro m;
+    @BeforeEach
+    public void setup() {
+        m = new Muro(6, 6, true);
+    }
     @org.junit.jupiter.api.Test
     void tipo() {
+        Assertions.assertEquals("Muro", m.tipo());
     }
 }
