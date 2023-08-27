0961c3c3bf27d0a4584ab57021276a790564608a
Y31low <samuele.giallorenzo@gmail.com>
2023-08-27 23:31:43 +0200

    Changes by Matrosna

--- Robotinho/Robotinho/src/Test/DirezioneTest.java
+++ Robotinho/Robotinho/src/Test/DirezioneTest.java
@@ -1,18 +1,31 @@
 package Test;
 
+import Game.Model.Direzione;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class DirezioneTest {
 
+    Direzione[] direzioni;
+
+    @BeforeEach
+    void setup(){
+        direzioni = new Direzione[4];
+        direzioni[0] = Direzione.North;
+        direzioni[1] = Direzione.East;
+        direzioni[2] = Direzione.West;
+        direzioni[3] = Direzione.South;
+    }
+
     @org.junit.jupiter.api.Test
     void randomDirection() {
-    }
-
-    @org.junit.jupiter.api.Test
-    void values() {
-    }
-
-    @org.junit.jupiter.api.Test
-    void valueOf() {
+        boolean check = false;
+        Direzione rand = Direzione.randomDirection();
+        for(Direzione dir:direzioni){
+            if(dir.equals(rand)) check = true;
+        }
+        Assertions.assertTrue(check);
     }
 }
